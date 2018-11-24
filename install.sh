9534d79bec2194c32f2994b9ac7144e18b917444
Zeeshan Khan <zee.khan34@gmail.com>
2018-11-24 16:32:17 +0500

    no message

--- install.sh
+++ install.sh
@@ -32,28 +32,6 @@ fi
 
 
 
-
-# #------------------------------------------------------
-# echo "installing bash aliases from dev-profile"
-# cd ~/zk-scripts
-# if [ ! -d ~/zk-scripts/dev-profile ]; then
-#     git clone https://github.com/dropocol/dev-profile.git
-#     cd ~/zk-scripts/dev-profile
-# else
-#     cd ~/zk-scripts/dev-profile
-#     git stash
-#     git stash clear
-#     git pull
-# fi
-
-# if ! grep -F "source ~/zk-scripts/dev-profile/.dev-profile" ~/.bashrc ; then
-#  echo 'source ~/zk-scripts/dev-profile/.dev-profile' >> ~/.bashrc
-# fi
-# . ~/.bashrc
-
-
-
-
 #------------------------------------------------------
 echo "installing git"
 sudo apt-get install -y git
@@ -194,6 +172,25 @@ echo "done"
 
 
 #------------------------------------------------------
+echo "installing bash aliases from dev-profile"
+cd ~/zk-scripts
+if [ ! -d ~/zk-scripts/dev-profile ]; then
+    git clone https://github.com/dropocol/dev-profile.git
+    cd ~/zk-scripts/dev-profile
+else
+    cd ~/zk-scripts/dev-profile
+    git stash
+    git stash clear
+    git pull
+fi
+
+if ! grep -F "source ~/zk-scripts/dev-profile/.dev-profile" ~/.bashrc ; then
+ echo 'source ~/zk-scripts/dev-profile/.dev-profile' >> ~/.bashrc
+fi
 . ~/.bashrc
+
+
+#------------------------------------------------------
+
 cd ~/tools/
 echo "done"
