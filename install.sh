cea1dd26b2ba0af3e6b568a88b14e3b34bd22851
Zeeshan Khan <zee.khan34@gmail.com>
2018-11-24 16:25:43 +0500

    no message

--- install.sh
+++ install.sh
@@ -32,6 +32,28 @@ fi
 
 
 
+
+#------------------------------------------------------
+echo "installing bash aliases from dev-profile"
+cd ~/zk-scripts
+if [ ! -d ~/zk-scripts/dev-profile ]; then
+    git clone https://github.com/dropocol/dev-profile.git
+    cd ~/zk-scripts/dev-profile
+else
+    cd ~/zk-scripts/dev-profile
+    git stash
+    git stash clear
+    git pull
+fi
+
+if ! grep -F "source ~/zk-scripts/dev-profile/.dev-profile" ~/.bashrc ; then
+ echo 'source ~/zk-scripts/dev-profile/.dev-profile' >> ~/.bashrc
+fi
+. ~/.bashrc
+
+
+
+
 #------------------------------------------------------
 echo "installing git"
 sudo apt-get install -y git
@@ -169,25 +191,6 @@ sudo echo "{ \"graph\" : \"/home/zeeshankhan/docker\"  }" | sudo tee --append /e
 sudo systemctl daemon-reload
 echo "done"
 
-
-
-#------------------------------------------------------
-echo "installing bash aliases from dev-profile"
-cd ~/zk-scripts
-if [ ! -d ~/zk-scripts/dev-profile ]; then
-    git clone https://github.com/dropocol/dev-profile.git
-    cd ~/zk-scripts/dev-profile
-else
-    cd ~/zk-scripts/dev-profile
-    git stash
-    git stash clear
-    git pull
-fi
-
-if ! grep -F "source ~/zk-scripts/dev-profile/.dev-profile" ~/.bashrc ; then
- echo 'source ~/zk-scripts/dev-profile/.dev-profile' >> ~/.bashrc
-fi
-. ~/.bashrc
 
 
 #------------------------------------------------------
